6d24c0e83ac093b55983664135a867cb463db690
srilakshmipotnuru <ep20btech11016@iith.ac.in>
2024-02-01 11:47:48 +0530

    changed files

--- underthehood.js
+++ underthehood.js
@@ -43,3 +43,20 @@ hsv.forEach(element => {
         HSVchange()
     })
 });
+
+
+function Hexchange(){
+    const hexval=document.getElementsByClassName('hex')
+    const val=hexval[0].value
+    console.log(val)
+
+}
+function RGBchange(){
+
+}
+function HSLchange(){
+
+}
+function HSVchange(){
+
+}
